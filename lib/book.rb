5deea13b80e329631dca399432af59c360980467
brysona10 <addisonbryson10@gmail.com>
2018-07-27 18:48:41 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,12 +4,12 @@
 class Book
   attr_reader :title 
   attr_accessor :author, :page_count, :genre
-def initialize (title)
-  @title = title
-end
-def turn_page
-  puts "Flipping the page...wow, you read fast!"
-end
+  def initialize(title)
+    @title = title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
   end
  
 the_great_gatsby = Book.new("The Great Gatsby")
